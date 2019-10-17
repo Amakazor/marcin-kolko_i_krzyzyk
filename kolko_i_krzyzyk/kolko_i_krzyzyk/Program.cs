be3e6dc4fc52b7bb65f85b58f31d1c8cdb67001b
Amakazor <amakazor@gmail.com>
2019-10-17 23:12:27 +0200

    basic variables

--- kolko_i_krzyzyk/kolko_i_krzyzyk/Program.cs
+++ kolko_i_krzyzyk/kolko_i_krzyzyk/Program.cs
@@ -6,7 +6,15 @@ namespace kolko_i_krzyzyk
     {
         static void Main(string[] args)
         {
+            char player_one_symbol  = 'O';
+            char player_two_symbol  = 'X';
+            char empty_symbol       = ' ';
 
+            int  size               = 3;
+
+            char[,] fields          = new char[size, size];
+
+            
         }
     }
 }
