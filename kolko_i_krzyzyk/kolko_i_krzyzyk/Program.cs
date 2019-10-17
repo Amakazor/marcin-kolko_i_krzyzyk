18ed0c44ef0548231af12b48d690b28eb906aa7b
Amakazor <amakazor@gmail.com>
2019-10-17 23:12:49 +0200

    render method

--- kolko_i_krzyzyk/kolko_i_krzyzyk/Program.cs
+++ kolko_i_krzyzyk/kolko_i_krzyzyk/Program.cs
@@ -4,6 +4,40 @@ namespace kolko_i_krzyzyk
 {
     class Program
     {
+        static void Render(char[,] fields_array)
+        {
+            Console.Write(" #");
+            for (int col = 0; col < fields_array.GetLength(1); col++)
+            {
+                Console.Write(col + "#");
+            }
+            Console.Write("\n");
+
+            for (int row = 0; row < fields_array.GetLength(0); row++)
+            {
+                Console.Write("##");
+                for (int col = 0; col < fields_array.GetLength(1); col++)
+                {
+                    Console.Write("##");
+                }
+                Console.Write("\n");
+
+                Console.Write(row + "#");
+                for (int col = 0; col < fields_array.GetLength(1); col++)
+                {
+                    Console.Write(fields_array[row, col] + "#");
+                }
+                Console.Write("\n");
+            }
+
+            Console.Write("##");
+            for (int col = 0; col < fields_array.GetLength(1); col++)
+            {
+                Console.Write("##");
+            }
+            Console.Write("\n");
+        }
+
         static void Main(string[] args)
         {
             char player_one_symbol  = 'O';
