4e77088011dbb94378a222e0a9eb9ccf27379c0a
Amakazor <amakazor@gmail.com>
2019-10-18 01:11:34 +0200

    game loop

--- kolko_i_krzyzyk/kolko_i_krzyzyk/Program.cs
+++ kolko_i_krzyzyk/kolko_i_krzyzyk/Program.cs
@@ -134,7 +134,90 @@ namespace kolko_i_krzyzyk
 
             int current_player = 1;
 
-            
+            int state = 0;
+
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    fields[row, col] = empty_symbol;
+                }
+            }
+
+            while (true)
+            {
+                int row;
+                int column;
+
+                bool wrong_field = false;
+
+                do
+                {
+                    Console.Clear();
+                    Render(fields);
+
+                    if (wrong_field) Console.WriteLine("Field not empty.");
+                    Console.WriteLine("Input field data");
+
+                    row = GetInputIntBeetween(1, 3, "Input row number") - 1;
+                    column = GetInputIntBeetween(1, 3, "Input column number") - 1;
+
+                    if (fields[row, column] != empty_symbol)
+                    {
+                        wrong_field = true;
+                    }
+                    else
+                    {
+                        wrong_field = false;
+                    }
+
+                } while (wrong_field);
+
+                if (current_player == 1)
+                {
+                    fields[row, column] = player_one_symbol;
+                    if (CheckIfWon(fields, player_one_symbol))
+                    {
+                        state = 1;
+                    }
+                }
+                else 
+                {
+                    fields[row, column] = player_two_symbol;
+                    if (CheckIfWon(fields, player_two_symbol))
+                    {
+                        state = 2;
+                    }
+                }
+
+                if (state == 0 && CheckIfFull(fields, empty_symbol))
+                {
+                    state = 3;
+                }
+
+                if (state != 0)
+                {
+                    break;
+                }
+                else
+                {
+                    if (current_player == 1) current_player = 2;
+                    else current_player = 1;
+                }
+            }
+
+            switch (state)
+            {
+                case 1:
+                    Console.WriteLine(player_one_symbol + " won!");
+                    break;
+                case 2:
+                    Console.WriteLine(player_two_symbol + " won!");
+                    break;
+                case 3:
+                    Console.WriteLine("Draw!");
+                    break;
+            }
         }
     }
 }
